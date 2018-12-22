aa08ab012447fd1bef8376b6dfd7fd5f71eb4b7d
DaryaChaplygina <das9884@gmail.com>
2018-12-22 13:40:59 +0300

    Reactor function for data preparation

--- model_learning/time_series_model.py
+++ model_learning/time_series_model.py
@@ -18,63 +18,53 @@ class TimeSeriesModel:
         return precision_recall_fscore_support(real, pred_class)
 
     def train_model(self, train: pd.DataFrame):
-        train_prof, train_tweet_all, train_labels = self.prepare_data(train)
-        profile_lr = LogisticRegression().fit(train_prof,
-                                                       train_labels)
+        prof_data, prof_labels, tweet_data, tweet_labels = \
+            self.prepare_data(train)
 
-        train_tweet, tweet_labels = self.__dropna_tweet(train_tweet_all,
-                                                        train_labels)
-        tweet_lr = LogisticRegression().fit(train_tweet,
-                                                     tweet_labels)
+        profile_lr = LogisticRegression().fit(prof_data, prof_labels.ravel())
+        tweet_lr = LogisticRegression().fit(tweet_data, tweet_labels.ravel())
+
         return profile_lr, tweet_lr
 
-    def prepare_data(self, data: pd.DataFrame):
-        profile_data, tweet_data, labels = self.__split_df(data)
-        labels = np.asarray([(0 if l == "genuine" else 1) for l in labels])
-        profile_data = np.asarray(
-            [[0, 1000000] if profile_data[i, 0] == 0 else profile_data[i, :]
-             for i in range(profile_data.shape[0])])   # replace inf
-
-        return profile_data, tweet_data, labels
-
     @staticmethod
-    def __split_df(data: pd.DataFrame):
-        profile_data = data[['following', 'followers_to_following_ratio']]
-        tweet_data = data[['links_per_tweet', 'unique_links_per_tweet',
-                           'usernames_per_tweet',
-                           'unique_usernames_per_tweet', 'tweets_entropy']]
-        labels = data[['label']]
-        return profile_data.values, tweet_data.values, labels.values
+    def prepare_data(data: pd.DataFrame):
+        profile_data = data[['following', 'followers_to_following_ratio']].copy()
+        profile_data.replace(np.inf, 1000000, inplace=True)
+        profile_labels = data[['label']].copy()
+        profile_labels = profile_labels.replace({"genuine": 0, "bot": 1})
 
-    @staticmethod
-    def __dropna_tweet(tweet_data: np.ndarray, labels: np.ndarray):
-        tweet = []
-        tweet_labels = []
-        for i in range(tweet_data.shape[0]):
-            if not np.isnan(tweet_data[i, 0]):
-                tweet.append(tweet_data[i, :])
-                tweet_labels.append(labels[i])
+        tweet_data_labeled = data[['links_per_tweet', 'unique_links_per_tweet',
+                                   'usernames_per_tweet', 'label',
+                                   'unique_usernames_per_tweet',
+                                   'tweets_entropy']].copy()
 
-        return np.asarray(tweet), np.asarray(tweet_labels)
+        # nan appears when no users tweets available
+        tweet_data_labeled.dropna(axis=0, how='any', inplace=True)
+        tweet_data = tweet_data_labeled.drop(labels=['label'], axis=1)
+        tweet_labels = tweet_data_labeled['label']
+        tweet_labels = tweet_labels.replace({"genuine": 0, "bot": 1})
+
+        return profile_data.values, profile_labels.values,\
+            tweet_data.values, tweet_labels.values
 
     def predict(self, test: pd.DataFrame, prof_lr, tweet_lr):
-        test_prof, test_tweet_all, test_labels = self.prepare_data(test)
-        prof_pred = prof_lr.predict_proba(test_prof)
+        prof_data, prof_labels, tweet_data, tweet_labels = \
+            self.prepare_data(test)
 
-        test_tweet, _ = self.__dropna_tweet(test_tweet_all, test_labels)
-        tweet_pred = tweet_lr.predict_proba(test_tweet)
+        prof_pred = prof_lr.predict_proba(prof_data)
+        tweet_pred = tweet_lr.predict_proba(tweet_data)
 
-        prediction = np.zeros((test_prof.shape[0], 2))
+        prediction = np.zeros((prof_data.shape[0], 2))
         tweet_idx = 0
-        for i in range(test_prof.shape[0]):
-            if np.isnan(test_tweet_all[i, 0]):
+        for i in range(prof_data.shape[0]):
+            if np.isnan(test['links_per_tweet'].iloc[i]):
                 prediction[i] = prof_pred[i]
             else:
                 prediction[i] = 0.5 * prof_pred[i] \
                                 + 0.5 * tweet_pred[tweet_idx]
                 tweet_idx += 1
 
-        return prediction, test_labels
+        return prediction, prof_labels
 
 
 if __name__ == "__main__":
